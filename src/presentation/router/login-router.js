00e787758af91767b4351709d0cf656d4f3b9bf2
Kilson <kilsonrs@gmail.com>
2020-12-23 15:17:59 -0300

    ensure LoginRouter returns 400 if an invalid email is provided

--- src/presentation/router/login-router.js
+++ src/presentation/router/login-router.js
@@ -1,12 +1,14 @@
 const HttpResponse = require('../helpers/http-response')
 const MissingParamError = require('../helpers/missing-param-error')
+const InvalidParamError = require('../helpers/invalid-param-error')
 
 module.exports = class LoginRouter {
-  constructor(authUseCase) {
+  constructor (authUseCase, emailValidator) {
     this.authUseCase = authUseCase
+    this.emailValidator = emailValidator
   }
 
-  async route(httpRequest) {
+  async route (httpRequest) {
     if (!httpRequest || !httpRequest.body || !this.authUseCase || !this.authUseCase.auth) {
       return HttpResponse.serverError()
     }
@@ -14,6 +16,9 @@ module.exports = class LoginRouter {
       const { email, password } = httpRequest.body
       if (!email) {
         return HttpResponse.badRequest(new MissingParamError('email'))
+      }
+      if (!this.emailValidator.isValid(email)) {
+        return HttpResponse.badRequest(new InvalidParamError('email'))
       }
       if (!password) {
         return HttpResponse.badRequest(new MissingParamError('password'))
