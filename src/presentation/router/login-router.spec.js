17695e83dff88cc2048afe5d44790b94ae9aa40d
Kilson <kilsonrs@gmail.com>
2020-12-23 15:37:22 -0300

    ensure LoginRouter calls EmailValidator with correct email

--- src/presentation/router/login-router.spec.js
+++ src/presentation/router/login-router.spec.js
@@ -18,7 +18,8 @@ const makeSut = () => {
 
 const makeEmailValidator = () => {
   class EmailValidatorSpy {
-    isValid () {
+    isValid (email) {
+      this.email = email
       return this.isEmailValid
     }
   }
@@ -95,6 +96,7 @@ describe('Login Router', () => {
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   })
+
   test('Should call AuthUseCase with correct params', async () => {
     const { sut, authUseCaseSpy } = makeSut()
     const httpRequest = {
@@ -235,5 +237,17 @@ describe('Login Router', () => {
     }
     const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Should call EmailValidator with correct email', async () => {
+    const { sut, emailValidatorSpy } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password'
+      }
+    }
+    await sut.route(httpRequest)
+    expect(emailValidatorSpy.email).toBe(httpRequest.body.email)
   })
 })
