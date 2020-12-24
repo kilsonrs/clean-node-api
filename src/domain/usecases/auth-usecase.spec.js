030ad57197488e06acaba0edbc6bc1f990228527
Kilson <kilsonrs@gmail.com>
2020-12-24 15:22:14 -0300

    remove unnecessary ifs

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,4 +1,4 @@
-const { MissingParamError, InvalidParamError } = require('../../utils/errors')
+const { MissingParamError } = require('../../utils/errors')
 
 const AuthUseCase = require('./auth-usecase')
 
@@ -38,7 +38,7 @@ describe('Auth UseCase', () => {
   test('Should throw if no LoadUserByEmailRepository is provided', async () => {
     const sut = new AuthUseCase()
     const promise = sut.auth('any_email@mail.com', 'any_password')
-    expect(promise).rejects.toThrow(new MissingParamError('loadUserByEmailRepository'))
+    expect(promise).rejects.toThrow()
   })
 
   test('Should throw if LoadUserByEmailRepository has no load method', async () => {
@@ -46,7 +46,7 @@ describe('Auth UseCase', () => {
     const loadUserByEmailRepository = new LoadUserByEmailRepository()
     const sut = new AuthUseCase(loadUserByEmailRepository)
     const promise = sut.auth('any_email@mail.com', 'any_password')
-    expect(promise).rejects.toThrow(new InvalidParamError('loadUserByEmailRepository'))
+    expect(promise).rejects.toThrow()
   })
 
   test('Should return null if LoadUserByEmailRepository returns null', async () => {
