4aec5e0ccc748e65f52287197149d69bc98c6832
Kilson <kilsonrs@gmail.com>
2020-12-25 16:51:42 -0300

    refact: change AuthUseCase contructor to receive an object

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -46,7 +46,11 @@ const makeSut = () => {
   const encrypterSpy = makeEncrypter()
   const tokenGeneratorSpy = makeTokenGenerator()
   const loadUserByEmailRepositorySpy = makeLoadUserByEmailRepository()
-  const sut = new AuthUseCase(loadUserByEmailRepositorySpy, encrypterSpy, tokenGeneratorSpy)
+  const sut = new AuthUseCase({
+    loadUserByEmailRepository: loadUserByEmailRepositorySpy,
+    encrypter: encrypterSpy,
+    tokenGenerator: tokenGeneratorSpy
+  })
   return {
     sut,
     encrypterSpy,
@@ -75,15 +79,15 @@ describe('Auth UseCase', () => {
   })
 
   test('Should throw if no LoadUserByEmailRepository is provided', async () => {
-    const sut = new AuthUseCase()
+    const sut = new AuthUseCase({})
     const promise = sut.auth('any_email@mail.com', 'any_password')
     expect(promise).rejects.toThrow()
   })
 
   test('Should throw if LoadUserByEmailRepository has no load method', async () => {
-    class LoadUserByEmailRepository {}
+    class LoadUserByEmailRepository { }
     const loadUserByEmailRepository = new LoadUserByEmailRepository()
-    const sut = new AuthUseCase(loadUserByEmailRepository)
+    const sut = new AuthUseCase({ loadUserByEmailRepository })
     const promise = sut.auth('any_email@mail.com', 'any_password')
     expect(promise).rejects.toThrow()
   })
